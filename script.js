304e6901c714cb45e441d707320ab2266aaf0942
BhavikaU <137325754+BhavikaU@users.noreply.github.com>
2023-11-07 13:25:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 function decimalToBinary(num) {
-  //Write you code here
+  // Initialize the output as an empty string
   let output = "";
-	while(num>=2)
-		{
-			let remainder = num % 2;
-			output = remainder + output;
-			num = parseInt(num/2);
-			
-		}
-	if(num === 1)
-	{
-		output  = 1+output;
-	}
-	console.log(output);
-	
+  while (num >= 2) {
+    let remainder = num % 2;
+    output = remainder + output;
+    num = parseInt(num / 2);
+  }
+  if (num === 1) {
+    output = 1 + output;
+  }
+  
+  return output; // Return the binary representation as a string
 }
 
 window.decimalToBinary = decimalToBinary;
